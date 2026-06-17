3b357e07d6ceb40cf8173bc52ba7a1765b7ce3f9
Shambhavi9228 <pshambhavi701@gmail.com>
2026-06-17 12:11:11 +0530

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
-def load_data(path):
-    df = pd.read_csv(path, sep=" ", header=None)
+def load_data(file_path):
+    df = pd.read_csv(file_path, sep=" ", header=None)
+    return df
+
+def preprocess_data(df):
+    df = df.dropna(axis=1, how='all')
     return df
