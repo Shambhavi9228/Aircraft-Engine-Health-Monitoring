c1f3389f50cc912574f6e400490f3d91be62ebe8
Shambhavi9228 <pshambhavi701@gmail.com>
2026-06-17 12:10:39 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -2,12 +2,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
 def build_model(input_shape):
-    model = Sequential([
-        LSTM(64, return_sequences=True, input_shape=input_shape),
-        LSTM(32),
-        Dense(16, activation='relu'),
-        Dense(1)
-    ])
+    model = Sequential()
+
+    model.add(LSTM(64, return_sequences=True, input_shape=input_shape))
+    model.add(LSTM(32))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(1))
 
     model.compile(
         optimizer='adam',
